6d36cbbc9378c9d3110bbd7a6a03cf1b9e8e38a6
Sascha Depold <sascha@depold.com>
2014-05-11 14:34:43 +0200

    Refactored test

--- test/init.test.js
+++ test/init.test.js
@@ -7,68 +7,65 @@ var expect    = require('expect.js')
   , path      = require('path')
   , os        = require('os')
   , cli       = "bin/sequelize"
+  ;
 
-if (os.type().toLowerCase().indexOf('windows') === -1) {
-  describe(Support.getTestDialectTeaser(cli), function() {
-    (function(flags) {
-      flags.forEach(function(flag) {
-        var cmd = cli + " " + flag
+([
+  'init'
+]).forEach(function(flag) {
+  var cwd = Support.resolveSupportPath('tmp')
 
-        describe(cmd, function() {
-          ;(function(folders) {
-            folders.forEach(function(folder) {
-              it("creates a '" + folder + "' folder", function(done) {
-                exec("rm -rf ./*", { cwd: __dirname + '/support/tmp' }, function() {
-                  exec("../../../" + cmd, { cwd: __dirname + '/support/tmp' }, function() {
-                    exec("ls -ila", { cwd: __dirname + '/support/tmp' }, function(err, stdout) {
-                      expect(stdout).to.contain(folder)
-                      done()
-                    })
-                  })
-                })
-              })
-            })
-          })(['config', 'migrations'])
-
-          it("creates a custom migrations folder", function(done) {
-            exec("rm -rf ./*", { cwd: __dirname + '/support/tmp' }, function() {
-              exec("../../../" + cmd + " --migrations-path ./db/migrate", { cwd: __dirname + '/support/tmp' }, function() {
-                exec("ls -ila", { cwd: __dirname + '/support/tmp' }, function(err, stdout) {
-                  expect(stdout).to.contain('db')
-                  done()
-                })
-              })
-            })
-          })
-
-          it("creates a config.json file", function(done) {
-            exec("rm -rf ./*", { cwd: __dirname + '/support/tmp' }, function() {
-              exec("../../../" + cmd, { cwd: __dirname + '/support/tmp' }, function() {
-                exec("ls -ila config", { cwd: __dirname + '/support/tmp' }, function(err, stdout) {
-                  expect(stdout).to.contain('config.json')
-                  done()
-                })
-              })
-            })
-          })
-
-          it("does not overwrite an existing config.json file", function(done) {
-            exec("rm -rf ./*", { cwd: __dirname + '/support/tmp' }, function() {
-              exec("../../../" + cmd, { cwd: __dirname + '/support/tmp' }, function() {
-                exec("echo 'foo' > config/config.json", { cwd: __dirname + '/support/tmp' }, function() {
-                  exec("../../../" + cmd, { cwd: __dirname + '/support/tmp' }, function(err) {
-                    expect(err.code).to.equal(1)
-                    exec("cat config/config.json", { cwd: __dirname + '/support/tmp' }, function(err, stdout) {
-                      expect(stdout).to.equal("foo\n")
-                      done()
-                    })
-                  })
-                })
+  describe(Support.getTestDialectTeaser(cli + " " + flag), function() {
+    ;(function(folders) {
+      folders.forEach(function(folder) {
+        it("creates a '" + folder + "' folder", function(done) {
+          exec("rm -rf ./*", { cwd: cwd }, function() {
+            exec(Support.getCliCommand(cwd, flag), { cwd: cwd }, function(err) {
+              exec("ls -ila", { cwd: cwd }, function(err, stdout) {
+                expect(stdout).to.contain(folder)
+                done()
               })
             })
           })
         })
       })
-    })(['init'])
+    })(['config', 'migrations'])
+
+    it("creates a custom migrations folder", function(done) {
+      exec("rm -rf ./*", { cwd: cwd }, function() {
+        exec(Support.getCliCommand(cwd, flag) + " --migrations-path ./db/migrate", { cwd: cwd }, function() {
+          exec("ls -ila", { cwd: cwd }, function(err, stdout) {
+            expect(stdout).to.contain('db')
+            done()
+          })
+        })
+      })
+    })
+
+    it("creates a config.json file", function(done) {
+      exec("rm -rf ./*", { cwd: __dirname + '/support/tmp' }, function() {
+        exec(Support.getCliCommand(cwd, flag), { cwd: cwd }, function() {
+          exec("ls -ila config", { cwd: cwd }, function(err, stdout) {
+            expect(stdout).to.contain('config.json')
+            done()
+          })
+        })
+      })
+    })
+
+    it("does not overwrite an existing config.json file", function(done) {
+      exec("rm -rf ./*", { cwd: cwd }, function() {
+        exec(Support.getCliCommand(cwd, flag), { cwd: cwd }, function() {
+          exec("echo 'foo' > config/config.json", { cwd: cwd }, function() {
+            exec(Support.getCliCommand(cwd, flag), { cwd: cwd }, function(err) {
+              expect(err.code).to.equal(1)
+              exec("cat config/config.json", { cwd: cwd }, function(err, stdout) {
+                expect(stdout).to.equal("foo\n")
+                done()
+              })
+            })
+          })
+        })
+      })
+    })
   })
-}
+})
