b0a05be21ec0c51cd0ea2cc3690362a8ef49fcb6
Sascha Depold <sascha@depold.com>
2014-06-13 10:32:58 +0200

    Rewrote undo tests

--- test/db/migrate/undo.test.js
+++ test/db/migrate/undo.test.js
@@ -1,79 +1,41 @@
 var expect    = require('expect.js')
   , Support   = require(__dirname + '/../../support')
-  , dialect   = Support.getTestDialect()
-  , _         = Support.Sequelize.Utils._
-  , exec      = require('child_process').exec
-  , version   = (require(__dirname + '/../../../package.json')).version
-  , path      = require('path')
-  , os        = require('os')
-  , cli       = "bin/sequelize"
+  , helpers   = require(__dirname + '/../../support/helpers')
+  , gulp      = require('gulp')
   ;
 
 ([
   'db:migrate:undo'
 ]).forEach(function(flag) {
-  var execBinary = function(callback, _flag) {
+  var prepare = function(callback, _flag) {
     _flag = _flag || flag
 
-    var dialect = Support.getTestDialect()
-      , config  = require(Support.resolveSupportPath('config', 'config.js'))
-      , cwd     = Support.resolveSupportPath('tmp')
-
-    config.sqlite.storage = Support.resolveSupportPath('tmp', 'test.sqlite')
-    config = _.extend(config, config[dialect], { dialect: dialect })
-
-    exec("echo '" + JSON.stringify(config) + "' > config/config.json", { cwd: cwd }, function(error, stdout) {
-      exec(Support.getCliCommand(cwd, _flag), { cwd: cwd }, callback)
-    })
+    gulp
+      .src(Support.resolveSupportPath('tmp'))
+      .pipe(helpers.clearDirectory())
+      .pipe(helpers.runCli('init'))
+      .pipe(helpers.copyMigration('createPerson.js'))
+      .pipe(helpers.overwriteFile(JSON.stringify(helpers.getTestConfig()), 'config/config.json'))
+      .pipe(helpers.runCli(_flag, { pipeStdout: true }))
+      .pipe(helpers.teardown(callback))
   }
 
-  var prepare = function(callback, options) {
-    var cwd = Support.resolveSupportPath('tmp')
-
-    options = options || {}
-
-    exec("rm -rf ./*", { cwd: cwd }, function(error, stdout) {
-      exec(Support.getCliCommand(cwd, 'init'), { cwd: cwd }, function(error, stdout) {
-        var migrationSource = Support.resolveSupportPath('assets', 'migrations')
-        exec("cp " + migrationSource + "/*-createPerson.js ./migrations/", { cwd: cwd }, function(error, stdout) {
-          if (!options.skipExecBinary) {
-            execBinary(callback, options.flag)
-          }
-        })
-      })
-    })
-  }
-
-  describe(Support.getTestDialectTeaser(cli + " " + flag), function() {
+  describe(Support.getTestDialectTeaser(flag), function() {
     it("creates a SequelizeMeta table", function(done) {
-      var sequelize = this.sequelize
-
-      if (this.sequelize.options.dialect === 'sqlite') {
-        var options = this.sequelize.options
-        options.storage = Support.resolveSupportPath('tmp', 'test.sqlite')
-        sequelize = new Support.Sequelize("", "", "", options)
-      }
+      var self = this
 
       prepare(function() {
-        sequelize.getQueryInterface().showAllTables().success(function(tables) {
+        self.sequelize.getQueryInterface().showAllTables().success(function(tables) {
           tables = tables.sort()
 
           expect(tables).to.have.length(1)
           expect(tables[0]).to.equal("SequelizeMeta")
           done()
         })
-      }.bind(this))
+      })
     })
 
     it("stops execution if no migrations have been done yet", function(done) {
-      var sequelize = this.sequelize
-
-      if (this.sequelize.options.dialect === 'sqlite') {
-        var options = this.sequelize.options
-        options.storage = Support.resolveSupportPath('tmp', 'test.sqlite')
-        sequelize = new Support.Sequelize("", "", "", options)
-      }
-
       prepare(function(err, output) {
         expect(err).to.be.null
         expect(output).to.contain("There are no pending migrations.")
@@ -81,32 +43,29 @@ var expect    = require('expect.js')
       }.bind(this))
     })
 
-    it("is correctly undoing a migration if they have been done yet", function(done) {
-      var sequelize = this.sequelize
+      it("is correctly undoing a migration if they have been done yet", function(done) {
+        var self = this
 
-      if (this.sequelize.options.dialect === 'sqlite') {
-        var options = this.sequelize.options
-        options.storage = Support.resolveSupportPath('tmp', 'test.sqlite')
-        sequelize = new Support.Sequelize("", "", "", options)
-      }
+        prepare(function() {
+          self.sequelize.getQueryInterface().showAllTables().success(function(tables) {
+            tables = tables.sort()
 
-      prepare(function() {
-        sequelize.getQueryInterface().showAllTables().success(function(tables) {
-          tables = tables.sort()
+            expect(tables).to.have.length(2)
+            expect(tables[0]).to.equal("Person")
 
-          expect(tables).to.have.length(2)
-          expect(tables[0]).to.equal("Person")
+            gulp
+              .src(Support.resolveSupportPath('tmp'))
+              .pipe(helpers.runCli(flag, { pipeStdout: true }))
+              .pipe(helpers.teardown(function(err, output) {
+                self.sequelize.getQueryInterface().showAllTables().success(function(tables) {
+                  expect(tables).to.have.length(1)
+                  expect(tables[0]).to.equal("SequelizeMeta")
 
-          execBinary(function(err, output) {
-            sequelize.getQueryInterface().showAllTables().success(function(tables) {
-              expect(tables).to.have.length(1)
-              expect(tables[0]).to.equal("SequelizeMeta")
-
-              done()
-            })
+                  done()
+                })
+              }))
           })
-        })
-      }.bind(this), { flag: 'db:migrate' })
-    })
+        }, 'db:migrate')
+      })
   })
 })
