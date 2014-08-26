815ffc75e1d264da14c48e89f39b710a88881857
Sascha Depold <sascha@depold.com>
2014-08-26 20:27:18 +0200

    Use gulp as task runner and make project jshint compatible.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,19 @@
-var gulp        = require('gulp')
-  , path        = require('path')
-  , fs          = require('fs')
-  , helpers     = require(path.resolve(__dirname, 'lib', 'helpers'))
+"use strict";
 
-require('gulp-help')(gulp, {
-  aliases: ['h'],
-  afterPrintCallback: helpers.gulp.printManuals
-})
+var gulp         = require("gulp");
+var jshint       = require("gulp-jshint");
+var path         = require("path");
 
-fs
-  .readdirSync(path.resolve(__dirname, 'lib', 'tasks'))
-  .filter(function(file) {
-    return file.indexOf('.') !== 0
-  })
-  .map(function(file) {
-    return require(path.resolve(__dirname, 'lib', 'tasks', file))
-  })
-  .forEach(function(tasks) {
-    Object.keys(tasks).forEach(function(taskName) {
-      helpers.gulp.addTask(gulp, taskName, tasks[taskName])
-      helpers.gulp.addHelp(gulp, taskName, tasks[taskName])
-    })
-  })
+gulp.task("default", ["lint"], function () {});
+
+gulp.task("lint", function () {
+  gulp
+    .src([
+      path.resolve(__dirname, "gulpfile.js"),
+      path.resolve(__dirname, "lib", "*.js"),
+      path.resolve(__dirname, "lib", "helpers", "*.js"),
+      path.resolve(__dirname, "lib", "tasks", "*.js")
+    ])
+    .pipe(jshint())
+    .pipe(jshint.reporter("default"));
+});
