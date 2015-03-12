itoed/sequelize-cli

Add option for JSON storage

lib/tasks/db.js
@@ -182,8 +182,8 @@ function getMigrator (callback) {
   if (helpers.config.configFileExists() || args.url) {
     var sequelize = getSequelizeInstance();
     var migrator  = new Umzug({
-      storage:        "sequelize",
-      storageOptions: { sequelize: sequelize },
+      storage:        helpers.umzug.getStorageMethod(),
+      storageOptions: helpers.umzug.getStorageOptions({ sequelize: sequelize }),
       logging:        console.log,
       migrations:     {
         params:  [ sequelize.getQueryInterface(), Sequelize ],

sequelize/cli

lib/tasks/db.js
@@ -182,8 +182,8 @@ function getMigrator (callback) {
   if (helpers.config.configFileExists() || args.url) {
     var sequelize = getSequelizeInstance();
     var migrator  = new Umzug({
-      storage:        "sequelize",
-      storageOptions: { sequelize: sequelize },
+      storage:        helpers.umzug.getStorage(),
+      storageOptions: helpers.umzug.getStorageOptions({ sequelize: sequelize }),
       logging:        console.log,
       migrations:     {
         params:  [ sequelize.getQueryInterface(), Sequelize ],