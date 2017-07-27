2160a5d6117f552f42252a2d46adf5fa286cc659
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2017-07-27 02:12:03 -0300

    Uncomment a test code that was incorrectly commented

--- test/migration/create.test.js
+++ test/migration/create.test.js
@@ -28,28 +28,28 @@ var _         = require('lodash');
         .pipe(helpers.teardown(callback));
     };
 
-    // it('creates a new file with the current timestamp', function (done) {
-    //   prepare(function () {
-    //     var date        = new Date();
-    //     var format      = function (i) {
-    //       return (parseInt(i, 10) < 10 ? '0' + i : i);
-    //     };
-    //     var sDate       = [
-    //       date.getFullYear(),
-    //       format(date.getMonth() + 1),
-    //       format(date.getDate()),
-    //       format(date.getHours()),
-    //       format(date.getMinutes())
-    //     ].join('');
-    //     var expectation = new RegExp(sDate + '..-' + migrationFile);
+    it('creates a new file with the current timestamp', function (done) {
+      prepare(function () {
+        var date        = new Date();
+        var format      = function (i) {
+          return (parseInt(i, 10) < 10 ? '0' + i : i);
+        };
+        var sDate       = [
+          date.getFullYear(),
+          format(date.getMonth() + 1),
+          format(date.getDate()),
+          format(date.getHours()),
+          format(date.getMinutes())
+        ].join('');
+        var expectation = new RegExp(sDate + '..-' + migrationFile);
 
-    //     gulp
-    //       .src(Support.resolveSupportPath('tmp', 'migrations'))
-    //       .pipe(helpers.listFiles())
-    //       .pipe(helpers.ensureContent(expectation))
-    //       .pipe(helpers.teardown(done));
-    //   });
-    // });
+        gulp
+          .src(Support.resolveSupportPath('tmp', 'migrations'))
+          .pipe(helpers.listFiles())
+          .pipe(helpers.ensureContent(expectation))
+          .pipe(helpers.teardown(done));
+      });
+    });
 
     it('adds a skeleton with an up and a down method', function (done) {
       prepare(function () {
