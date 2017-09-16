654af216cff12dfafa287e2ca4ca30b1743788a4
Sushant <sushantdhiman@outlook.com>
2017-09-16 17:04:12 +0530

    added: db:create / db:drop tasks

--- src/sequelize.js
+++ src/sequelize.js
@@ -28,9 +28,9 @@ import seedOne from './commands/seed_one';
 import migrationGenerate from './commands/migration_generate';
 import modelGenerate from './commands/model_generate';
 import seedGenerate from './commands/seed_generate';
+import database from './commands/database';
 
 import helpers from './helpers/index';
-
 
 helpers.view.teaser();
 
@@ -44,6 +44,8 @@ const cli = yargs
   .command('db:seed:undo', 'Deletes data from the database', seedOne)
   .command('db:seed:all', 'Run every seeder', seed)
   .command('db:seed:undo:all', 'Deletes data from the database', seed)
+  .command('db:create', 'Create database specified by configuration', database)
+  .command('db:drop', 'Drop database specified by configuration', database)
   .command('init', 'Initializes project', init)
   .command('init:config', 'Initializes configuration', init)
   .command('init:migrations', 'Initializes migrations', init)
---
+++ test/db/db-create.test.js
@@ -0,0 +1,64 @@
+const expect = require('expect.js');
+const Support = require(__dirname + '/../support');
+const helpers = require(__dirname + '/../support/helpers');
+const gulp = require('gulp');
+const _ = require('lodash');
+
+const prepare = function (flag, callback, options) {
+  options = _.assign({ config: {} }, options || {});
+
+  const configPath = 'config/config.json';
+  const config = _.assign({}, helpers.getTestConfig(), options.config);
+  const configContent = JSON.stringify(config);
+
+  gulp
+    .src(Support.resolveSupportPath('tmp'))
+    .pipe(helpers.clearDirectory())
+    .pipe(helpers.runCli('init'))
+    .pipe(helpers.removeFile(configPath))
+    .pipe(helpers.overwriteFile(configContent, configPath))
+    .pipe(helpers.runCli(flag, { pipeStdout: true }))
+    .pipe(helpers.teardown(callback));
+};
+
+describe(Support.getTestDialectTeaser('db:create'), () => {
+  if (Support.dialectIsPostgres()) {
+    it('correctly creates database', function (done) {
+      const databaseName = `my_test_db_${_.random(10000, 99999)}`;
+      prepare(
+        'db:create',
+        () => {
+          this.sequelize.query(`SELECT 1 as exists FROM pg_database WHERE datname = '${databaseName}';`, {
+            type: this.sequelize.QueryTypes.SELECT
+          }).then(result => {
+            expect(result[0].exists).to.eql(1);
+            done();
+          });
+        }, {
+          config: {
+            database: databaseName
+          }
+        });
+    });
+  }
+
+  if (Support.dialectIsMySQL()) {
+    it('correctly creates database', function (done) {
+      const databaseName = `my_test_db_${_.random(10000, 99999)}`;
+      prepare(
+        'db:create',
+        () => {
+          this.sequelize.query(`SELECT IF('${databaseName}' IN(SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA), 1, 0) AS found;`, {
+            type: this.sequelize.QueryTypes.SELECT
+          }).then(result => {
+            expect(result[0].found).to.eql(1);
+            done();
+          });
+        }, {
+          config: {
+            database: databaseName
+          }
+        });
+    });
+  }
+});
