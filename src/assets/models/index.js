1e72f8108812b0c82f726fbf8a56db281d5f8b37
Jonathan Julian <jonathan@jonathanjulian.com>
2017-11-10 13:33:53 -0800

    Pass the full config to the Sequelize constructor [Fixes #584]

--- src/assets/models/index.js
+++ src/assets/models/index.js
@@ -9,7 +9,7 @@ var config    = require(<%= configFile %>)[env];
 var db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
