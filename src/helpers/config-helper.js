8d524548c4fce8c03e95500e1d6f7b8f6149c1f0
Sushant <sushantdhiman@outlook.com>
2017-12-03 14:07:10 +0530

    Revert "change: better defaults for production environment (#579)"
    This reverts commit 006c4ec6161e3706bf86ddcd598e06ab3e0ea9c9.

--- src/helpers/config-helper.js
+++ src/helpers/config-helper.js
@@ -81,10 +81,10 @@ const api = {
         dialect: 'mysql'
       },
       production: {
-        username: process.env.PROD_DB_USERNAME,
-        password: process.env.PROD_DB_PASSWORD,
-        database: process.env.PROD_DB_NAME,
-        host: process.env.PROD_DB_HOSTNAME,
+        username: 'root',
+        password: null,
+        database: 'database_production',
+        host: '127.0.0.1',
         dialect: 'mysql'
       }
     }, undefined, 2) + '\n';
