993625b6327c82b8f5f28777f8025edd4e6a187c
Kenny Peng <kepeng@amplify.com>
2018-03-25 23:04:45 -0400

    Attempt to create a custom schema if dialect is Postgres and it doesn't exist yet

--- src/core/migrator.js
+++ src/core/migrator.js
@@ -61,6 +61,24 @@ export function getMigrator (type, args) {
 
     return sequelize
       .authenticate()
+      .then(() => {
+        // Check if this is a PostgreSQL run and if there is a custom schema specified, and if there is, check if it's
+        // been created. If not, attempt to create it.
+        if (helpers.version.getDialectName() === 'pg') {
+          const customSchemaName = helpers.umzug.getSchema('migration');
+          if (customSchemaName && customSchemaName !== 'public') {
+            return sequelize.showAllSchemas().then(schemas => {
+              if (!_.includes(schemas, customSchemaName)) {
+                return sequelize.createSchema(customSchemaName);
+              } else {
+                return Bluebird.resolve();
+              }
+            });
+          }
+        }
+
+        return Bluebird.resolve();
+      })
       .then(() => migrator)
       .catch(e => helpers.view.error(e));
   });
@@ -127,7 +145,8 @@ export function addTimestampsToSchema (migrator) {
             }
           }, {
             tableName,
-            timestamps: true
+            timestamps: true,
+            schema: helpers.umzug.getSchema()
           });
 
           return SequelizeMeta.sync()
