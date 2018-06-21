8aa474fdf36d223e54c3ccf7879a96d7abd3a901
Adeel Hussain <mr.adi180@gmail.com>
2018-06-21 13:19:16 +0500

    Set timestamp in migration file if timestamp flag is not falsy

--- src/helpers/migration-helper.js
+++ src/helpers/migration-helper.js
@@ -4,24 +4,30 @@ import helpers from './index';
 const Sequelize = helpers.generic.getSequelize();
 
 module.exports = {
-  getTableName (modelName) {
+  getTableName(modelName) {
     return Sequelize.Utils.pluralize(modelName);
   },
 
-  generateTableCreationFileContent (args) {
-    return helpers.template.render('migrations/create-table.js', {
-      tableName:  this.getTableName(args.name),
-      attributes: helpers.model.transformAttributes(args.attributes),
-      createdAt:  args.underscored ? 'created_at' : 'createdAt',
-      updatedAt:  args.underscored ? 'updated_at' : 'updatedAt'
-    });
+  generateTableCreationFileContent(args) {
+    const templateOptions = {
+      tableName: this.getTableName(args.name),
+      attributes: helpers.model.transformAttributes(args.attributes)
+    };
+
+
+    //If timestamp argument is not false or is not set then add timestamp 
+    if (args.timestamp !== false) {
+      templateOptions.createdAt = args.underscored ? 'created_at' : 'createdAt';
+      templateOptions.updatedAt = args.underscored ? 'updated_at' : 'updatedAt';
+    }
+    return helpers.template.render('migrations/create-table.js', templateOptions);
   },
 
-  generateMigrationName (args) {
+  generateMigrationName(args) {
     return _.trimStart(_.kebabCase('create-' + args.name), '-');
   },
 
-  generateTableCreationFile (args) {
+  generateTableCreationFile(args) {
     const migrationName = this.generateMigrationName(args);
     const migrationPath = helpers.path.getMigrationPath(migrationName);
 
