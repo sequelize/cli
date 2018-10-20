aedca525bc403afc64643df1285c115911f48d93
Wei-Liang Liou <me@aliangliang.top>
2018-10-20 16:07:55 +0800

    feat(db:create): support options on db:create with sequelize@4 (#700)

--- test/db/db-create.test.js
+++ test/db/db-create.test.js
@@ -58,6 +58,58 @@ describe(Support.getTestDialectTeaser('db:create'), () => {
           }
         });
     });
+
+    it('correctly creates database with encoding, collate and template', function (done) {
+      const databaseName = `my_test-db_${_.random(10000, 99999)}`;
+      prepare(
+        'db:create --encoding UTF8 --collate zh_TW.UTF-8 --template template0',
+        () => {
+          this.sequelize.query(`SELECT
+           1 as exists,
+           pg_encoding_to_char(encoding) as encoding,
+           datcollate as collate,
+           datctype as ctype
+           FROM pg_database WHERE datname = '${databaseName}';`, {
+            type: this.sequelize.QueryTypes.SELECT
+          }).then(result => {
+            expect(result[0].exists).to.eql(1);
+            expect(result[0].encoding).to.eql('UTF8');
+            expect(result[0].collate).to.eql('zh_TW.UTF-8');
+            expect(result[0].ctype).to.eql('en_US.utf8');
+            done();
+          });
+        }, {
+          config: {
+            database: databaseName
+          }
+        });
+    });
+
+    it('correctly creates database with encoding, collate, ctype and template', function (done) {
+      const databaseName = `my_test-db_${_.random(10000, 99999)}`;
+      prepare(
+        'db:create --encoding UTF8 --collate zh_TW.UTF-8 --ctype zh_TW.UTF-8 --template template0',
+        () => {
+          this.sequelize.query(`SELECT
+           1 as exists,
+           pg_encoding_to_char(encoding) as encoding,
+           datcollate as collate,
+           datctype as ctype
+           FROM pg_database WHERE datname = '${databaseName}';`, {
+            type: this.sequelize.QueryTypes.SELECT
+          }).then(result => {
+            expect(result[0].exists).to.eql(1);
+            expect(result[0].encoding).to.eql('UTF8');
+            expect(result[0].collate).to.eql('zh_TW.UTF-8');
+            expect(result[0].ctype).to.eql('zh_TW.UTF-8');
+            done();
+          });
+        }, {
+          config: {
+            database: databaseName
+          }
+        });
+    });
   }
 
   if (Support.dialectIsMySQL()) {
@@ -88,6 +140,51 @@ describe(Support.getTestDialectTeaser('db:create'), () => {
             type: this.sequelize.QueryTypes.SELECT
           }).then(result => {
             expect(result[0].found).to.eql(1);
+            done();
+          });
+        }, {
+          config: {
+            database: databaseName
+          }
+        });
+    });
+
+    it('correctly creates database with charset', function (done) {
+      const databaseName = `my_test-db_${_.random(10000, 99999)}`;
+      prepare(
+        'db:create --charset utf8mb4',
+        () => {
+          this.sequelize.query(`SELECT
+            DEFAULT_CHARACTER_SET_NAME as charset,
+            DEFAULT_COLLATION_NAME as collation
+            FROM information_schema.SCHEMATA WHERE schema_name = '${databaseName}';`, {
+            type: this.sequelize.QueryTypes.SELECT
+          }).then(result => {
+            expect(result[0].charset).to.eql('utf8mb4');
+            expect(result[0].collation).to.eql('utf8mb4_general_ci');
+            done();
+          });
+        }, {
+          config: {
+            database: databaseName
+          }
+        });
+    });
+  
+
+    it('correctly creates database with charset and collation', function (done) {
+      const databaseName = `my_test-db_${_.random(10000, 99999)}`;
+      prepare(
+        'db:create --charset utf8mb4 --collate utf8mb4_unicode_ci',
+        () => {
+          this.sequelize.query(`SELECT
+            DEFAULT_CHARACTER_SET_NAME as charset,
+            DEFAULT_COLLATION_NAME as collation
+            FROM information_schema.SCHEMATA WHERE schema_name = '${databaseName}';`, {
+            type: this.sequelize.QueryTypes.SELECT
+          }).then(result => {
+            expect(result[0].charset).to.eql('utf8mb4');
+            expect(result[0].collation).to.eql('utf8mb4_unicode_ci');
             done();
           });
         }, {
