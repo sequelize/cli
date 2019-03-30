a329d7490b3e1bf074b764d5bf2ba239f254e4c6
Simon Schick <demwizzy@gmail.com>
2019-03-30 22:03:36 +0100

    chore(models): update templates

--- src/assets/models/model.js
+++ src/assets/models/model.js
@@ -1,18 +1,19 @@
 'use strict';
 
-module.exports = (sequelize, DataTypes) => {
-  const <%= name %> = sequelize.define('<%= name %>', {
+module.exports = (sequelize, DataTypes, Model) => {
+  class <%= name %> extends Model {
+    static associate (models) {
+      // associations can be defined here
+    }
+  }
+
+  return <%= name %>.init({
     <% attributes.forEach(function(attribute, index) { %>
       <%= attribute.fieldName %>: DataTypes.<%= attribute.dataFunction ? `${attribute.dataFunction.toUpperCase()}(DataTypes.${attribute.dataType.toUpperCase()})` : attribute.dataValues ? `${attribute.dataType.toUpperCase()}(${attribute.dataValues})` : attribute.dataType.toUpperCase() %>
       <%= (Object.keys(attributes).length - 1) > index ? ',' : '' %>
     <% }) %>
   }, {
+    sequelize,
     <%= underscored ? 'underscored: true,' : '' %>
   });
-
-  <%= name %>.associate = function(models) {
-    // associations can be defined here
-  };
-
-  return <%= name %>;
 };
