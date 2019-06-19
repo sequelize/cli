ab94e6084b10d583862f6d3cbd0d51a4df41cf18
Dmitry Onishchenko <sacr8tum@gmail.com>
2019-06-19 11:27:11 +0300

    Fixed db:seed:undo

--- src/commands/seed_one.js
+++ src/commands/seed_one.js
@@ -3,6 +3,7 @@ import { getMigrator } from '../core/migrator';
 
 import helpers from '../helpers';
 import path from 'path';
+import _ from 'lodash';
 
 exports.builder =
   yargs =>
@@ -34,12 +35,35 @@ exports.handler = async function (args) {
       break;
 
     case 'db:seed:undo':
-      await getMigrator('seeder', args).then(migrator => {
-        return migrator.down({ migrations: seeds });
-      }).catch(e => helpers.view.error(e));
+      await seedUndo(args);
       break;
   }
 
   process.exit(0);
 };
 
+function seedUndo (args) {
+  return getMigrator('seeder', args).then(migrator => {
+    return (
+      helpers.umzug.getStorage('seeder') === 'none' ? migrator.pending() : migrator.executed()
+    )
+      .then(seeders => {
+        if (args.seed) {
+          seeders = seeders.filter(seed => {
+            return args.seed.includes(seed.file);
+          });
+        }
+
+        if (seeders.length === 0) {
+          helpers.view.log('No seeders found.');
+          return;
+        }
+
+        if (!args.seed) {
+          seeders = seeders.slice(-1);
+        }
+
+        return migrator.down({ migrations: _.chain(seeders).map('file').reverse().value() });
+      });
+  }).catch(e => helpers.view.error(e));
+}
