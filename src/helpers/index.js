5d3a7cdd6aa2941c6454f56be8948875b0b019fe
steveraider <sriram@poshmark.com>
2019-10-11 17:48:22 +0530

    change generic helper load order

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -7,7 +7,7 @@ fs
   .readdirSync(__dirname)
   .filter(file => file.indexOf('.') !== 0 && file.indexOf('index.js') === -1)
   .forEach(file => {
-    module.exports[file.replace('-helper.js', '')] = require(path.resolve(__dirname, file));
+    module.exports[file.replace('-helper.js', '').replace('_', '')] = require(path.resolve(__dirname, file));
   });
 
 module.exports.default = module.exports;
