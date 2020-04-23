a08e6366a1feef7774ad5a36ada3cdd4b94d916e
Leonardo Negreiros <negreirosleo12@gmail.com>
2020-04-23 19:58:45 -0300

    Remove operatorsAliases from the default config

--- src/helpers/config-helper.js
+++ src/helpers/config-helper.js
@@ -72,7 +72,6 @@ const api = {
         database: 'database_development',
         host: '127.0.0.1',
         dialect: 'mysql',
-        operatorsAliases: false
       },
       test: {
         username: 'root',
@@ -80,7 +79,6 @@ const api = {
         database: 'database_test',
         host: '127.0.0.1',
         dialect: 'mysql',
-        operatorsAliases: false
       },
       production: {
         username: 'root',
@@ -88,7 +86,6 @@ const api = {
         database: 'database_production',
         host: '127.0.0.1',
         dialect: 'mysql',
-        operatorsAliases: false
       }
     }, undefined, 2) + '\n';
   },
