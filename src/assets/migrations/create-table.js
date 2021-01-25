833711e3a8443af6caddd3c3bc8b2cb10c2da048
Mike <mrgcohen@gmail.com>
2021-01-25 14:12:35 -0500

    If using createdAt/updatedAt default to now

--- src/assets/migrations/create-table.js
+++ src/assets/migrations/create-table.js
@@ -19,11 +19,13 @@ module.exports = {
       <%= createdAt %>: {
         allowNull: false,
         type: Sequelize.DATE
+        defaultValue: Sequelize.NOW,
       },
 
       <%= updatedAt %>: {
         allowNull: false,
         type: Sequelize.DATE
+        defaultValue: Sequelize.NOW,
       }
     });
   },
