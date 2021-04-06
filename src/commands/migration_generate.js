95b7f24d6aaf0b80a5066b733111e870565def5c
Higor Bocutti <higor_rafael.sp@hotmail.com>
2021-04-06 01:48:48 -0300

    fix: bug #157 when try generate migration but retrieve an error like no such file

--- src/commands/migration_generate.js
+++ src/commands/migration_generate.js
@@ -4,6 +4,11 @@ import helpers from '../helpers';
 import fs from 'fs';
 import clc from 'cli-color';
 
+const ext = {
+  ts: 'ts',
+  js: 'js',
+};
+
 exports.builder = (yargs) =>
   _underscoreOption(
     _baseOptions(yargs).option('name', {
@@ -15,11 +20,14 @@ exports.builder = (yargs) =>
 
 exports.handler = function (args) {
   helpers.init.createMigrationsFolder();
+  const path = fs.existsSync(`migrations/skeleton.${ext.ts}`)
+    ? `migrations/skeleton.${ext.ts}`
+    : `migrations/skeleton.${ext.js}`;
 
   fs.writeFileSync(
     helpers.path.getMigrationPath(args.name),
     helpers.template.render(
-      'migrations/skeleton.js',
+      path,
       {},
       {
         beautify: false,
