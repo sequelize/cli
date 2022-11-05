6ba32e1fde860c537402fe022226a3da16e7f6a8
Luciano Ursache <paul@cobalt.io>
2022-11-05 12:06:54 +0100

    feat: add support for test files colocated with models (#1187)

--- src/assets/models/index.js
+++ src/assets/models/index.js
@@ -19,7 +19,12 @@ if (config.use_env_variable) {
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
